3b9aed2145022263f540e0ee11345d8973c79a37
Kavipriya <kavipriya.p004@gmail.com>
2026-02-27 18:03:42 +0530

    Update db.js

--- Backend/src/models/db.js
+++ Backend/src/models/db.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "resume_job_portal",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
